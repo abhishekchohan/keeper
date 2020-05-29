e85c1e20b466afa725dc3c3ca0feab54fc11da6e
Abhishek Chohan <abhishekchohan1@gmail.com>
2020-05-29 02:50:19 -0400

    Updates

---
+++ precache-manifest.ba79e7778f2abf81e03acf22ba7d8422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0af3ff927fec7db3c132b16cca3d863",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "18fd21414590f469e0a0",
+    "url": "/keeper/static/js/2.a9926fb4.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/keeper/static/js/2.a9926fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd859dec5ce950c7b48",
+    "url": "/keeper/static/js/main.72ccbe33.chunk.js"
+  },
+  {
+    "revision": "a0fe8f1cafe3e4405f4e",
+    "url": "/keeper/static/js/runtime-main.534cd8d7.js"
+  }
+]);
